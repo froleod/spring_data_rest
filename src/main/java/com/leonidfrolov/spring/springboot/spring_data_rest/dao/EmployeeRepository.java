957f2e2868613cb2d12fafa8eb2f5df301acde95
Leonid Frolov <froleod@gmail.com>
2023-08-25 20:14:22 +0300

    Initial commit

---
+++ src/main/java/com/leonidfrolov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.leonidfrolov.spring.springboot.spring_data_rest.dao;
+
+
+import com.leonidfrolov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
